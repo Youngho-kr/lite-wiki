e81233b1ef888332d5dbc3bd4ddd7c4c96976beb
Youngho-kr <62059540+Youngho-kr@users.noreply.github.com>
2025-05-16 17:12:26 +0900

    fix: docker setup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -10,12 +10,12 @@ if [ ! -f /data/settings.json ]; then
     cp /lite-wiki/data/settings.json /data/settings.json
 fi
 
-if [ ! "$(ls -A /data/docs)" ]; then
-    cp -r /lite-wiki/data/docs/* /data/docs/
+if [ -d /lite-wiki/data/docs ] && [ -z "$(ls -A /data/docs 2>/dev/null)" ]; then
+    cp -r /lite-wiki/data/docs/* /data/docs/ || true
 fi
 
-if [ ! "$(ls -A /data/uploads)" ]; then
-    cp -r /lite-wiki/data/uploads/* /data/uploads/
+if [ -d /lite-wiki/data/uploads ] && [ -z "$(ls -A /data/uploads 2>/dev/null)" ]; then
+    cp -r /lite-wiki/data/uploads/* /data/uploads/ || true
 fi
 
 exec lite-wiki
